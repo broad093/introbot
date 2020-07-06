ab619b9675bfdfdc505e520262d8728d5e6937d3
meijiishinislame <meijiishinislame@gmail.com>
2020-07-05 14:00:04 -1000

    Everything working, TODO add embeds and playing

--- bot.py
+++ bot.py
@@ -21,6 +21,9 @@ def is_botadmin(ctx):
 def make_mention_object_by_id(author_id):
 	return "<@{}>".format(author_id)
 
+def is_mention(input):
+	return input.startswith("<@!")
+
 
 
 ########################### BOOT ########################### 
@@ -39,28 +42,36 @@ async def on_ready():
 @bot.command(name='intro', pass_context=True)
 async def get_intro(ctx, target_user):
 	try:
-		converter = commands.UserConverter()
-		target_user = await converter.convert(ctx, target_user)
-		await send_intro(ctx.author, target_user)
+		if is_mention(target_user):
+			converter = commands.UserConverter()
+			target_user = await converter.convert(ctx, target_user)
+		else:
+			target_user = await string_to_user(ctx, target_user) #target user can be a string
+		await send_intro(ctx, target_user)
 	except Exception as e:
 		print(e)
 		await ctx.channel.send(content="Could not fetch intro.")
 
 async def get_intro(target_user):
 	intro_channel = bot.get_channel(INTRO_CHANNEL_ID)
-	async for message in intro_channel.history(limit=200):
+	async for message in intro_channel.history(limit=300):
 		if message.author == target_user:
 			return target_user.name, message.content
 
-async def send_intro(ctx_user, target_user):
-	username, message = await get_intro(target_user)
-	dm_message_content = "**{}**. \n--------------------------------------\n{}\n--------------------------------------".format(username, message)
-	await ctx_user.send(dm_message_content)
+async def send_intro(ctx, target_user):
+	try:
+		username, message = await get_intro(target_user)
+		dm_message_content = "**{}**. \n--------------------------------------\n{}\n--------------------------------------".format(username, message)
+		await ctx.author.send(dm_message_content)
+	except Exception as e:
+		print(e)
+		await ctx.channel.send("User has not made an intro yet!")
 
-async def string_to_user(string_to_convert):
-	for user in all_users:
-		if string_to_convert == user.nick or string_to_convert == user.name:
-			return user
+async def string_to_user(ctx, string_to_convert):
+	string_to_convert = string_to_convert.lower()
+	for member in ctx.guild.members:
+		if string_to_convert == str(member.nick).lower() or string_to_convert == str(member.name).lower():
+			return member
 
 
 
