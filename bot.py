f0b3989d7d8bc019e5bf2332eadcb9531832ce39
meijiishinislame <meijiishinislame@gmail.com>
2020-11-08 14:03:03 -1000

    debugging

--- bot.py
+++ bot.py
@@ -71,7 +71,7 @@ async def get_intro(ctx, *,  target_user):
 
 async def get_intro(target_user):
 	intro_channel = guild.get_channel(INTRO_CHANNEL_ID)
-	message_list = await intro_channel.history(limit=1000).flatten()
+	message_list = await intro_channel.history(limit=400).flatten()
 	message_list.reverse() #reverse to get first post
 
 	for message in message_list:
